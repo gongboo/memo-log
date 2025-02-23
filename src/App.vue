e7850556a65ab6b1ec7304e7ed9c050c81f84dda
gongboo <53379511+gongboo@users.noreply.github.com>
2025-02-24 01:57:25 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld />
 </template>
 
 <script>
@@ -19,7 +18,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="log-board">
+      <textarea 
+        v-model="txt"
+        class="log-textarea"
+      ></textarea>
+    </div>
+
+    <select v-model="selectedOp" name="options">
+      <option value="ACT" selected>ACT</option>
+      <option value="ERR">ERR</option>
+      <option value="INFO">INFO</option>
+    </select>
+    <input 
+      v-model="inputTxt" 
+      type="text" 
+      @keyup.enter.prevent="handleSubmit"
+    >
+    <button @click="handleSubmit">enter</button>
   </div>
 </template>
 
@@ -35,24 +26,49 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      count: 0,
+      txt: 'texts',
+      inputTxt: '',
+      selectedOp:'ACT',
+      currentTime: ''
+    }
+  },
+  methods: {
+    getTime() {
+      const now = new Date();
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      const seconds = String(now.getSeconds()).padStart(2, '0');
+      
+      this.currentTime = `${hours}:${minutes}:${seconds}`;
+      return this.currentTime;
+    },
+    // handleChange() { },
+    handleSubmit(event) {
+      if (event) event.preventDefault();  // 이벤트 기본 동작 방지
+
+      this.txt += "\n[" + this.getTime() +"]"+ this.selectedOp +"-"+ this.inputTxt;
+      this.inputTxt = '';
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.log-textarea {
+  width: 300px;
+  height: 200px;
+  resize: none;
+  overflow-y: auto;
+  padding: 10px;
+  font-family: monospace;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.pre-wrap {
+  white-space: pre-wrap;
 }
 </style>
